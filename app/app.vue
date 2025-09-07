b0147fb5e8aad998ec746f13c06b0fa2642b5416
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-09-07 03:15:16 -0600

    feat: Create base layouts and theme, add form components and dependencies

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,20 @@
 <template>
-  <!-- <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div> -->
-  <NuxtPage />
+  <!-- <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout> -->
+  <div>
+    <NuxtLoadingIndicator
+      :height="3"
+      color="var(--brand)"
+      :throttle="20"
+    />
+    <NuxtLayout>
+      <slot />
+    </NuxtLayout>
+  </div>
 </template>
 <script lang="ts" setup></script>
+<style>
+  body {
+  }
+</style>
